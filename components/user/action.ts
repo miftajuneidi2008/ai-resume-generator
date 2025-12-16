93fc02e4ef76deeb48a9294f647410026e8ed774
unknown <DELL 7020@ZZB-TRANS-PC1.zamzambank.local>
2025-12-16 14:47:31 +0300

    add resume

--- components/user/action.ts
+++ components/user/action.ts
@@ -5,13 +5,13 @@ import prisma from "@/lib/prisma";
 import { ResumeType } from "@/lib/ValidationSchema";
 import { redirect } from "next/navigation";
 
-export const createResume = async (values: ResumeType) => {
+export const saveResume = async (values: ResumeType) => {
   const session = await getUserSession();
   if (!session) {
     return redirect("/login");
   }
+  console.log(values);
   const { id } = values;
-  console.log("recieced values", id);
   const { photo, workExperience, education, ...resumeVaues } = values;
   const existingResume = id
     ? await prisma.resume.findUnique({
@@ -21,5 +21,54 @@ export const createResume = async (values: ResumeType) => {
 
   if (id && !existingResume) {
     throw new Error("Resume not found");
+  }
+
+  if (id) {
+    return await prisma.resume.update({
+      where: { id, userId: session?.user.id },
+      data: {
+        ...resumeVaues,
+        photoUrl: photo,
+        WorkExperience: {
+          deleteMany: {},
+          create: workExperience?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        Education: {
+          deleteMany: {},
+          create: education?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+        updatedAt: new Date(),
+      },
+    });
+  } else {
+    return await prisma.resume.create({
+      data: {
+        ...resumeVaues,
+        photoUrl: photo,
+        userId: session?.user.id,
+        WorkExperience: {
+          create: workExperience?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        Education: {
+          create: education?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+      },
+    });
   }
 };
--- feature/user/components/autoSaveResume.ts
+++ feature/user/components/autoSaveResume.ts
@@ -1,26 +1,73 @@
+import { saveResume } from "@/components/user/action";
 import useDebounce from "@/hooks/useDebounce";
 import { ResumeType } from "@/lib/ValidationSchema";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-export default function autoSaveResume(resumeData: ResumeType) {
+export default function useAutoSaveResume(resumeData: ResumeType) {
+  const searchParams = useSearchParams();
+
   const debouncedResumeData = useDebounce(resumeData, 1500);
+  const [resumeId, setResumeId] = useState(resumeData.id);
   const [lastSavedData, setLastSavedData] = useState(
     structuredClone(resumeData),
   );
   const [isSaving, setIsSaving] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setError(false);
+  }, [debouncedResumeData]);
+
   useEffect(() => {
     async function save() {
-      setIsSaving(true);
-      await new Promise((resolve) => setTimeout(resolve, 1500));
-      setLastSavedData(structuredClone(debouncedResumeData));
-      setIsSaving(false);
+      try {
+        setIsSaving(true);
+        setError(false);
+        const newData = structuredClone(debouncedResumeData);
+
+        const upadatedResume = await saveResume({
+          ...newData,
+          ...(lastSavedData.photo?.toString() === newData.photo?.toString() && {
+            photo: undefined,
+          }),
+          id: resumeId,
+        });
+
+        setResumeId(upadatedResume.id);
+        setLastSavedData(newData);
+        if (searchParams.get("resumeId") !== upadatedResume.id) {
+          const newSearchParams = new URLSearchParams(searchParams);
+          newSearchParams.set("resumeId", upadatedResume.id);
+          window.history.replaceState(
+            null,
+            "",
+            `?${newSearchParams.toString()}`,
+          );
+        }
+      } catch (error: any) {
+        setError(true);
+        toast.error(error || "Something went wrong");
+      } finally {
+        setIsSaving(false);
+      }
     }
+
     const hasUnsavedChangeData =
       JSON.stringify(lastSavedData) !== JSON.stringify(debouncedResumeData);
-    if (hasUnsavedChangeData && !isSaving && debouncedResumeData) {
+
+    if (hasUnsavedChangeData && !isSaving && debouncedResumeData && !error) {
       save();
     }
-  }, [debouncedResumeData, lastSavedData, isSaving]);
+  }, [
+    debouncedResumeData,
+    lastSavedData,
+    isSaving,
+    resumeId,
+    error,
+    searchParams,
+  ]);
 
   return {
     isSaving,
